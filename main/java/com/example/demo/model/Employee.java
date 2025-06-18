efb619b1dbe8cba9530e2721a56918b3c0800599
Abishek228 <abishek.rk.228@gmail.com>
2025-06-18 16:54:22 +0530

    Add files via upload
    new

---
+++ main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,67 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Column(name = "first_name", nullable = false)
+    private String firstName;
+    public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Column(name = "last_name", nullable = false)
+    private String lastName;
+    public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Column(name = "email_address", nullable = false)
+    private String emailId;
+
+    public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+     
+
+}
